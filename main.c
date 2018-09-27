8bae377f80996f5e9690c7840e1aef3a00fa3374
Samuel Magaña <38167725+s11magna@users.noreply.github.com>
2018-09-26 17:25:23 -0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,23 @@
+#include "msp.h"
+#include "delay.h"
+
+/**
+ * main.c
+ */
+void main(void)
+{
+	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+
+	//Code for Oscilloscope
+	//Mux Select
+	P4->SEL0 |= (BIT3); // set to 1
+	P4->SEL1 &= ~(BIT3); //set to 0
+
+	//Bit Flip direction out
+	P4->DIR |= (BIT3);   //set as output
+
+	setDCO(FREQ_3_MHZ);
+
+
+}
+
