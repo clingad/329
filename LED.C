c4461523d0ffab386c1e76d0409cf7b1830b20f0
Samuel Magaña <38167725+s11magna@users.noreply.github.com>
2018-10-01 16:16:50 -0700

    Add files via upload

--- LED.C
+++ LED.C
@@ -7,99 +7,104 @@
 #include "msp.h"
 #include "LED.H"
 #include "delay.h"
+#include "string.h"
 
-void sendOut(char i){
-    if(BIT_CHECK(i,7)){
-        P4->OUT |= BIT3;
-    }
-    else{
-        P4->OUT &= ~BIT3;
-    }
+  // P3.0 = RS
+    // P3.2 = r/w
+    // P3.3 = E
+    // P4.1 = DB4
+    // P4.2 = DB5
+    // P4.3 = DB6
+    // P4.4 = DB7
 
-    if(BIT_CHECK(i,6)){
-        P4->OUT |= BIT2;
-    }
-    else{
-        P4->OUT &= ~BIT2;
-    }
-
-    if(BIT_CHECK(i,5)){
-        P4->OUT |= BIT1;
-    }
-    else{
-        P4->OUT &= ~BIT1;
-    }
-
-    if(BIT_CHECK(i,4)){
-        P4->OUT |= BIT0;
-     }
-     else{
-         P4->OUT &= ~BIT0;
-     }
+// Write Char LCD takes in the character the line and
+// position in the line you want the character to be set in
+void write_char_LCD(char i, int line, int pos)
+{
+ int j;
+ cursorSet(line,pos);
+ j = i >> 4; // send upper bits
+ P4->OUT = j;
+ P3->OUT |= (RS);  // send data
+ P3->OUT &= ~(RW); // write
+ Nybble(); //Clock lower 4 bits
+ P4->OUT = i; // send lower bits
+ Nybble(); //Clock upper 4 bits
+ delay_ms(1);
 
 }
+/**********************************************************/
+// Returns Cursor back to the top left line
+void home_LCD(){
+    command(RETURNCURSOR);
+     delay_ms(2);// return to upper left part of display
+}
+// clears LCD
+void clear_LCD(){
+    command(CLEARDISPLAY);
+    delay_ms(2);
+}
+
+// sends commands to the LCD
 void command(char i)
 {
- sendOut(i); //put data on output Port
- P3->OUT &= ~(BIT0 | BIT2);
+ int j;
+ j = i >> 4;
+ P4->OUT = j; // send upper bits
+ P3->OUT &= ~(RS | RW);
  Nybble(); //Send lower 4 bits
- i = i << 4; //Shift over by 4 bits
- sendOut(i); //put data on output Port
+ P4->OUT = i;  // Send Lower bits
  Nybble(); //Send upper 4 bits
  delay_us(40);
 }
-/**********************************************************/
-
-
-void write(char i)
-{
- sendOut(i); //put data on output Port
-// D_I = 1; //D/I=HIGH : send data
- //R_W = 0; //R/W=LOW : Write
- P3->OUT |= (BIT0);
- P3->OUT &= ~( BIT2);
- Nybble(); //Clock lower 4 bits
- i = i << 4; //Shift over by 4 bits
- sendOut(i); //put data on output Port
- Nybble(); //Clock upper 4 bits
-
-}
-/**********************************************************/
+// enable pulse
 void Nybble()
 {
-/* E = 1;
- Delay(1); //enable pulse width >= 300ns
- E = 0; //Clock enable: falling edge*/
 
-     P3->OUT |= BIT3;  // Set E High
+     P3->OUT |= ENABLE;  // Set E High
      delay_us(0);
-     P3->OUT &= ~BIT3; // E LOW
-
+     P3->OUT &= ~ENABLE; // E LOW
 
  }
+// base address is 0x80 for 1st line
+// second line base address is 0xC0
+// the length of a line is 0 to 15
+void cursorSet(int line,int pos){ // first line is 0 // second line 1
+    int address = FIRSTLINE;
+        if(line >= 0 && line < 2){
+            if(pos >= 0 && pos < 16){
+                if(line == 1){
+                    address += SECONDLINE;
+                }
+                address += pos;
+            }
+            // writing off the screen
+        }
+        command(address);
+}
+
  /**********************************************************/
+
  void init()
  {
-     P3->SEL0 &= ~(BIT0 | BIT2 | BIT3);// Where is 3.1?
-        P3->SEL1 &= ~(BIT0 | BIT2 | BIT3);
-        P4->SEL0 = 0x00;
-        P4->SEL1 = 0x00;
-        P3->DIR |= (BIT0 | BIT2 | BIT3);
-        P4->DIR = 0x0F;
+    P3->SEL0 &= ~(RS | RW | ENABLE);// Where is 3.1?
+    P3->SEL1 &= ~(RS | RW | ENABLE);
+    P4->SEL0 = 0x00;
+    P4->SEL1 = 0x00;
+    P3->DIR |= (RS | RW | ENABLE);
+    P4->DIR = 0x0F; // P4.0 to P4.3
+
   delay_ms(40); //Wait >40 msec after power is applied
-  sendOut(0x03);//put 0x30 on the output port
+  P4->OUT = 0x03;//put 0x03 on the output port
   delay_ms(5); //must wait 5ms, busy flag not available
   Nybble(); //command 0x30 = Wake up #3
   delay_us(40);//can check busy flag now instead of delay
-  sendOut(0x02); //put 0x20 on the output port
+  P4->OUT = 0x02; //put 0x02 on the output port
   delay_us(40); //Function set: 4-bit interface
   command(0x28); //Function set: 4-bit/2-line
-  command(0x10); //Set cursor
+  command(CURSOR); //Set cursor
   command(0x0F); //Display ON; Blinking cursor
-   //command(0x10); // clear display
   command(0x06); //Entry Mode set
 
 }
-
-
 
--- main.c
+++ main.c
@@ -15,13 +15,17 @@
     // P4.4 = DB7
 int main() {
 
+    int i;
     WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;
     setDCO(FREQ_24_MHZ);
-    init();
-    //command(0x01);
-    command(0xC2);
-    write(0x30);
 
+  init(); // initalize the LCD
+  clear_LCD(); // clear the lcd
+  write_char_LCD('A', 0,0x0); // 'A' in the top left first line
+  write_char_LCD('B', 1,15); // write B in the lower right. The second line
+  write_char_LCD('F',0,15);
+  write_char_LCD('S',1,0x00);
+  home_LCD(); // return the cursor to the top left without erasing what is currently written
 
 }
 /**********************************************************/
