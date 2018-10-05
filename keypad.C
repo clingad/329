48b1dd9fa77f6b6411d2e3aa8102fcd9e20d8ee9
Samuel Magaña <38167725+s11magna@users.noreply.github.com>
2018-10-05 12:44:37 -0700

    Add files via upload
    comments added

--- keypad.C
+++ keypad.C
@@ -6,23 +6,19 @@
  */
 
 
-//p4.4  Col 1 pin 1
-//p1.5  Col 2 pin 3
-//p4.5  Col 3 pin 5
-//p4.6  row 1 pin 2
-//p4.7  row 2 pin 4
-//p6.5  row 3 pin 6
-//p5.4  row 4 pin 7
+//p6.5  Col 1 pin 1
+//p6.6  Col 2 pin 3
+//p6.7  Col 3 pin 5
+//p2.4  row 1 pin 2
+//p2.5  row 2 pin 4
+//p2.6  row 3 pin 6
+//p2.7  row 4 pin 7
 
 #include "keypad.h"
 #include "delay.h"
 void initKeypad(){
 
-    //GPIO
-    //ROWS
-  //  P2->SEL0 &= ~(ROW1 | ROW2 | ROW3 | ROW4);
- //   P2->SEL1 &= ~(ROW1 | ROW2 | ROW3 | ROW4);
-
+   //GPIO
     // set ROWS as input
     P2->DIR &= ~(ROW1 | ROW2 | ROW3 | ROW4); // input
 
@@ -43,31 +39,42 @@ uint8_t keypad_getkey(){
         int row = 0;
         int key = 0;
         // put all the COLS high then delay to make sure
-        // that a bit actually went high
+        // that they actually went high
         P6->OUT |= (COL1|COL2|COL3);
         delay_us(0);
+        // BIT MASK
         row = P2->IN & (ROW1|ROW2|ROW3|ROW4);
-        // check to see if any of bits is 1
+        // check to see if any of the inputs in row is 1
         if(row == 0){
             return 0xFF;
-        } // if one of the bits is high then // find the column
+        }
+        // if one of the bits is high then
+        // find the column
         else{
 
                 for(col = 0; col < 3; col++){
+                    // turn them all off
                     P6->OUT &= ~(COL1|COL2|COL3);
                     // now we need to test each column
+                    // by turning each one on
                     P6->OUT |= (COL1 << col);
                     delay_us(0);
+                    // check all the rows at that certain column
                     row = P2->IN & (ROW1|ROW2|ROW3|ROW4);
+
+                    // if it is on then stop the for loop
                     if(row != 0){
                         break;
                     }
                 }
+                // Turn off all columns
                 P6->OUT &= ~(COL1|COL2|COL3);
 
-                if(col == 3){
-                    return 0xFF;
+                if(col == 3){ //input missed
+                    return INVALID;
                 }
+                // reassign each row value in order
+                // to calculate the key
                 if(row == 16){
                     row = 1;
                 }
@@ -81,6 +88,8 @@ uint8_t keypad_getkey(){
                      row = 4;
                  }
 
+
+            // use the row and column to calculate the key
             if(col == 0){
                 key = (row*3-2) + '0';
             }
